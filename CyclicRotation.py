f681c5575534d8a547ef1c2ee106e35761912eba
Dongin <gang601@naver.com>
2017-02-04 16:12:17 +0900

    Add files via upload

---
+++ CyclicRotation.py
@@ -0,0 +1,17 @@
+#-*- coding:utf-8 -*-
+
+'''
+ A = [3, 8, 9, 7, 6]
+ K = 3
+ return [9, 7, 6, 3, 8]
+'''
+
+def solution(A, K):
+    B = A[:]  # A 리스트 복사
+    
+    for i in range(0,len(A)):
+        B[(i+K)%len(A)] = A[i] # 핵심 알고리즘  
+    
+    return B
+    
+    
