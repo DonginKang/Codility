cd9a0bc03aba0bdc9a7db884151d4833a44d3b7c
Dongin <gang601@naver.com>
2017-02-09 18:03:32 +0900

    Add files via upload

---
+++ lck.py
@@ -0,0 +1,8 @@
+You end up with much more discrete and readable classes
+This is an excerpt from the report "Network Automation with Ansible," by Jason Edelman. 
+The modules in this repo are officially deprecated
+Ansible does not require a dedicated server to be used. In fact, many machines could have Ansible installed and they can be used to simultaneuously automate any given environment
+Inventory is a collection of hosts against which Ansible can work with.
+Ansible can works with metadata from various sources and manage their context in the form of variables.
+Plays are ordered sets of tasks to execute against host selections from your inventory
+A playbook is a file containnig one or more plays.
