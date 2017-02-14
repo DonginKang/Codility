32066e7c509a7a26a4eabe8f093b27fd3282a815
DonginKang <gang601@naver.com>
2017-02-15 08:48:25 +0900

    Update Permcheck.py

--- Permcheck.py
+++ Permcheck.py
@@ -8,9 +8,9 @@ Check whether array A is a permutation.
 
 def solution(A):
 	A.sort()
-	if not A[0] == 1:         
+	if not A[0] == 1:            
 		return 0
-	for i in xrange(len(A)):      # 내가 실수한 부분 len(A)-1 로 썻음.. 
+	for i in xrange(len(A)):      #  My mistake is len(A)-1 ... 
 		if not A[i] == i+1:
 			return 0
 			
