899027ecc1d007c6d00779d7ee8b33432b9ec56c
DonginKang <gang601@naver.com>
2017-02-20 04:01:57 +0000

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+
+	
+A = [3,4,4,6,1,4,4]
+N = 5
+
+def solution(N, A):
+	B = [0] * N
+	for i in xrange(len(A)):
+		if A[i] <= N:
+			B[A[i]-1] += 1
+		else:
+			C = sorted(B)
+			for j in xrange(len(B)):
+				B[j] = C[N-1]
+	return B
+	
+print solution(N,A)
