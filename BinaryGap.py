d11b635fbfa8cf623267425cef47ca1d60d65825
DonginKang <gang601@naver.com>
2017-02-23 17:47:48 +0900

    Update BinaryGap.py

--- BinaryGap.py
+++ BinaryGap.py
@@ -2,7 +2,7 @@
 
 # Find longest sequence of zeros in binary representation of an integer.
 
-# int -> bin 변환을 위한 함수
+# decimal -> binary 변환을 위한 함수
 def int2bin(i):
     binary = ""
     if i < 0:
@@ -18,16 +18,16 @@ def int2bin(i):
     
     
 def solution(N):
-    bin = int2bin(N) # bin 은 String 형태 
+    binary = int2bin(N) # bin 은 String 형태 
     GapLength = 0  #  1과 1 사이의 0의 길이 값을 저장하는 변수
     max = 0 # 여러 GapLength 들 중 가장 큰 값을 저장하는 변수
    
-    for pos in range(0,len(bin)-1):
-        if bin[pos] == '1' and bin[pos+1] == '0':
+    for pos in range(0,len(binary)-1):
+        if binary[pos] == '1' and binary[pos+1] == '0':
             GapLength += 1
-        elif bin[pos] == '0' and bin[pos+1] == '0':
+        elif binary[pos] == '0' and binary[pos+1] == '0':
             GapLength += 1
-        elif bin[pos] == '0' and bin[pos+1] == '1':
+        elif binary[pos] == '0' and binary[pos+1] == '1':
             if GapLength > max:
                 max = GapLength
             GapLength = 0
